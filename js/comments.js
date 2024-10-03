edb2411f5bf1e1269648421bec0bf1ae1f6a0eff
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2024-10-03 14:45:56 +0600

    finish

---
+++ js/comments.js
@@ -0,0 +1,17 @@
+const loadComment = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('error happened',error))
+}
+
+const loadComment2= async () =>{
+    try{
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments');
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error('error khaisi mama',error);
+    }
+}
